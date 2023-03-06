fdc1d5c171f23469177b568bc28fe53c830ac980
Louie <louiejaycantres@gmail.com>
2023-03-06 09:10:46 +0800

    my first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <title>Open Forum</title>
+</head>
+<body>
+
+    <div class="container d-flex justify-content-center flex-column">
+
+        <h1 class="text-black-50 text-center mt-5">Open Forum</h1>
+
+        <header class="container mt-5">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="#">:)</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Discussions</a>
+                      </li>
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Dropdown
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <li><a class="dropdown-item" href="#">Action</a></li>
+                          <li><a class="dropdown-item" href="#">Another action</a></li>
+                          <li><hr class="dropdown-divider"></li>
+                          <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                    <form class="d-flex">
+                      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                      <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                  </div>
+                </div>
+              </nav>
+        </header>
+
+        <section class="container mt-5">
+
+            <div class="border-bottom mt-5 d-flex flex-column">
+                <div class="d-flex ">
+                    <div class="p-3  justify-content-center align-items-center text-center d-none d-md-flex">
+                        <p>:)</p>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <div>
+                            <h4 class="d-flex"> 
+                                <div  class="d-flex justify-content-center align-items-center text-center d-block d-md-none pe-3">
+                                    <p>:)</p> 
+                                </div>
+                                <a href="http://" class="text-decoration-none">This is some heading</a> 
+                            </h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor magni praesentium similique officia quibusdam excepturi quo minima esse, quisquam in at expedita quasi. Enim cumque quas at, tempora illo obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-end py-2">
+                    <p class="text-black-50 fw-light px-2">3k likes</p>
+                    <p class="text-warning px-2">Like</p>
+                    <p class="text-warning px-2">Comment</p>
+                </div>
+            </div>
+            <div class="border-bottom mt-5 d-flex flex-column">
+                <div class="d-flex ">
+                    <div class="p-3  justify-content-center align-items-center text-center d-none d-md-flex">
+                        <p>:)</p>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <div>
+                            <h4 class="d-flex"> 
+                                <div  class="d-flex justify-content-center align-items-center text-center d-block d-md-none pe-3">
+                                    <p>:)</p> 
+                                </div>
+                                <a href="http://" class="text-decoration-none">This is some heading</a> 
+                            </h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor magni praesentium similique officia quibusdam excepturi quo minima esse, quisquam in at expedita quasi. Enim cumque quas at, tempora illo obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-end py-2">
+                    <p class="text-black-50 fw-light px-2">3k likes</p>
+                    <p class="text-warning px-2">Like</p>
+                    <p class="text-warning px-2">Comment</p>
+                </div>
+            </div>
+            <div class="border-bottom mt-5 d-flex flex-column">
+                <div class="d-flex ">
+                    <div class="p-3  justify-content-center align-items-center text-center d-none d-md-flex">
+                        <p>:)</p>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <div>
+                            <h4 class="d-flex"> 
+                                <div  class="d-flex justify-content-center align-items-center text-center d-block d-md-none pe-3">
+                                    <p>:)</p> 
+                                </div>
+                                <a href="http://" class="text-decoration-none">This is some heading</a> 
+                            </h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor magni praesentium similique officia quibusdam excepturi quo minima esse, quisquam in at expedita quasi. Enim cumque quas at, tempora illo obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-end py-2">
+                    <p class="text-black-50 fw-light px-2">3k likes</p>
+                    <p class="text-warning px-2">Like</p>
+                    <p class="text-warning px-2">Comment</p>
+                </div>
+            </div>
+            <div class="border-bottom mt-5 d-flex flex-column">
+                <div class="d-flex ">
+                    <div class="p-3  justify-content-center align-items-center text-center d-none d-md-flex">
+                        <p>:)</p>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <div>
+                            <h4 class="d-flex"> 
+                                <div  class="d-flex justify-content-center align-items-center text-center d-block d-md-none pe-3">
+                                    <p>:)</p> 
+                                </div>
+                                <a href="http://" class="text-decoration-none">This is some heading</a> 
+                            </h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor magni praesentium similique officia quibusdam excepturi quo minima esse, quisquam in at expedita quasi. Enim cumque quas at, tempora illo obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-end py-2">
+                    <p class="text-black-50 fw-light px-2">3k likes</p>
+                    <p class="text-warning px-2">Like</p>
+                    <p class="text-warning px-2">Comment</p>
+                </div>
+            </div>
+            <div class="border-bottom mt-5 d-flex flex-column">
+                <div class="d-flex ">
+                    <div class="p-3  justify-content-center align-items-center text-center d-none d-md-flex">
+                        <p>:)</p>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <div>
+                            <h4 class="d-flex"> 
+                                <div  class="d-flex justify-content-center align-items-center text-center d-block d-md-none pe-3">
+                                    <p>:)</p> 
+                                </div>
+                                <a href="http://" class="text-decoration-none">This is some heading</a> 
+                            </h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor magni praesentium similique officia quibusdam excepturi quo minima esse, quisquam in at expedita quasi. Enim cumque quas at, tempora illo obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-end py-2">
+                    <p class="text-black-50 fw-light px-2">3k likes</p>
+                    <p class="text-warning px-2">Like</p>
+                    <p class="text-warning px-2">Comment</p>
+                </div>
+            </div>
+            <div class="border-bottom mt-5 d-flex flex-column">
+                <div class="d-flex ">
+                    <div class="p-3  justify-content-center align-items-center text-center d-none d-md-flex">
+                        <p>:)</p>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <div>
+                            <h4 class="d-flex"> 
+                                <div  class="d-flex justify-content-center align-items-center text-center d-block d-md-none pe-3">
+                                    <p>:)</p> 
+                                </div>
+                                <a href="http://" class="text-decoration-none">This is some heading</a> 
+                            </h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor magni praesentium similique officia quibusdam excepturi quo minima esse, quisquam in at expedita quasi. Enim cumque quas at, tempora illo obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-end py-2">
+                    <p class="text-black-50 fw-light px-2">3k likes</p>
+                    <p class="text-warning px-2">Like</p>
+                    <p class="text-warning px-2">Comment</p>
+                </div>
+            </div>
+            <div class="border-bottom mt-5 d-flex flex-column">
+                <div class="d-flex ">
+                    <div class="p-3  justify-content-center align-items-center text-center d-none d-md-flex">
+                        <p>:)</p>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <div>
+                            <h4 class="d-flex"> 
+                                <div  class="d-flex justify-content-center align-items-center text-center d-block d-md-none pe-3">
+                                    <p>:)</p> 
+                                </div>
+                                <a href="http://" class="text-decoration-none">This is some heading</a> 
+                            </h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor magni praesentium similique officia quibusdam excepturi quo minima esse, quisquam in at expedita quasi. Enim cumque quas at, tempora illo obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-end py-2">
+                    <p class="text-black-50 fw-light px-2">3k likes</p>
+                    <p class="text-warning px-2">Like</p>
+                    <p class="text-warning px-2">Comment</p>
+                </div>
+            </div>
+
+        </section>
+
+    </div>
+    
+
+</body>
+</html>
